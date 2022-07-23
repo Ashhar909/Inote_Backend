a53901246069ef03804273a95b4be3cd5c88571e
Ashhar909 <ashhar374@gmail.com>
2022-07-23 19:37:27 +0530

    sign up faults fixed and edit fixed

--- middleware/fetchUser.js
+++ middleware/fetchUser.js
@@ -3,17 +3,17 @@ const jwt = require('jsonwebtoken');
 const JWT_SECRET = 'Ashh$$$';
 
 const fetchUser = (req,res,next)=>{
-    // pass the auth token as a body
-    const token = req.header('auth-token')
+    // pass the auth token as a header
+    const token = req.header("auth-token")
     if(!token){
         res.status(401).send({error: "use a valid token"})
     }
     try {
         const data = jwt.verify(token,JWT_SECRET);
-        req.user = data.user;   // pass the data as a req
+        req.user = data.user;   
+        // pass the data as a req
         
     } catch (error) {
-        res.setHeader("auth-token", token)
         console.log(error.message)
         res.status(500).json({error:"internal server error"})
     }
